7e0d4f92014f66215b1639ec6d4d658355e58760
goztrk <me@gokhan.org.tr>
2023-08-01 19:53:48 +0300

    Add font families

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,20 @@
-import type { Metadata } from 'next'
-import '@/app/scss/style.scss'
 import React from 'react'
+import type { Metadata } from 'next'
+import { Montserrat, Merriweather } from 'next/font/google'
+import '@/app/scss/style.scss'
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-montserrat',
+  display: 'swap',
+})
+const merriweather = Merriweather({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-merriweather',
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   title: 'Contrarian.mba',
@@ -13,7 +27,10 @@ type Props = {
 
 export default function RootLayout({ children }: Props) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className={`${montserrat.variable} ${merriweather.variable}`}
+    >
       <body>{children}</body>
     </html>
   )
