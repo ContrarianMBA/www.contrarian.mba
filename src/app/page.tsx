608eea816bfd76beb3e97e55e05d9d6e3cb43b07
Gökhan Öztürk <1487670+goztrk@users.noreply.github.com>
2023-08-01 19:54:37 +0300

    Add font families (#3)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <main>Hello, World!</main>
+  return (
+    <main>
+      <h1>Hello, World!</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore
+        minima ea atque quas sunt laudantium, nihil suscipit reprehenderit non
+        doloremque dolorem totam hic sapiente rem culpa aut autem quibusdam.
+        Atque!
+      </p>
+    </main>
+  )
 }
