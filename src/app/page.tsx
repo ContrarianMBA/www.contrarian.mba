d71c6a5851cb41dfcef0fa78c5beb7714a25d933
goztrk <me@gokhan.org.tr>
2023-08-01 23:44:29 +0300

    Add header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
+'use client'
+import { Container } from 'react-bootstrap'
+
 export default function Home() {
   return (
     <main>
-      <h1>Hello, World!</h1>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore
-        minima ea atque quas sunt laudantium, nihil suscipit reprehenderit non
-        doloremque dolorem totam hic sapiente rem culpa aut autem quibusdam.
-        Atque!
-      </p>
+      <Container>
+        <h1>Hello, World!</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore
+          minima ea atque quas sunt laudantium, nihil suscipit reprehenderit non
+          doloremque dolorem totam hic sapiente rem culpa aut autem quibusdam.
+          Atque!
+        </p>
+      </Container>
     </main>
   )
 }
