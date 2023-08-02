e8f0ec8aa1ac33fae4941758d5c347f6bf52cf01
Gökhan Öztürk <1487670+goztrk@users.noreply.github.com>
2023-08-02 13:29:35 +0300

    ✨ feat: add footer (#5)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,14 @@
 'use client'
 import React from 'react'
+
 import { Montserrat, Merriweather } from 'next/font/google'
+
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
-import { Header } from '@/components/Header'
 import '@/scss/style.scss'
+
+import { Header } from '@/components/Header'
+import { Footer } from '@/components/Footer'
 
 config.autoAddCss = false
 
@@ -41,6 +45,7 @@ export default function RootLayout({ children }: Props) {
       <body className="d-flex flex-column vh-100">
         <Header theme={theme} toggleTheme={toggleTheme} />
         <main className="flex-grow-1">{children}</main>
+        <Footer />
       </body>
     </html>
   )
