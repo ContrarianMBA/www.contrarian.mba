6a88c11da2b6306309aa69092ebd762378704e2f
goztrk <me@gokhan.org.tr>
2023-08-03 15:27:42 +0300

    🔨 Increase tab with to 4 and add semi colons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,52 +1,52 @@
-'use client'
-import React from 'react'
+'use client';
+import React from 'react';
 
-import { Montserrat, Merriweather } from 'next/font/google'
+import { Montserrat, Merriweather } from 'next/font/google';
 
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-import '@/scss/style.scss'
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import '@/scss/style.scss';
 
-import { Header } from '@/components/Header'
-import { Footer } from '@/components/Footer'
+import { Header } from '@/components/Header';
+import { Footer } from '@/components/Footer';
 
-config.autoAddCss = false
+config.autoAddCss = false;
 
 const montserrat = Montserrat({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  variable: '--font-montserrat',
-  display: 'swap',
-})
+    subsets: ['latin'],
+    weight: ['400', '700'],
+    variable: '--font-montserrat',
+    display: 'swap',
+});
 const merriweather = Merriweather({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  variable: '--font-merriweather',
-  display: 'swap',
-})
+    subsets: ['latin'],
+    weight: ['400', '700'],
+    variable: '--font-merriweather',
+    display: 'swap',
+});
 
 type Props = {
-  children: React.ReactNode
-}
+    children: React.ReactNode;
+};
 
 export default function RootLayout({ children }: Props) {
-  const [theme, setTheme] = React.useState<'light' | 'dark'>('light')
+    const [theme, setTheme] = React.useState<'light' | 'dark'>('light');
 
-  const toggleTheme = () => {
-    setTheme(theme === 'light' ? 'dark' : 'light')
-  }
+    const toggleTheme = () => {
+        setTheme(theme === 'light' ? 'dark' : 'light');
+    };
 
-  return (
-    <html
-      lang="en"
-      className={`${montserrat.variable} ${merriweather.variable}`}
-      data-bs-theme={theme}
-    >
-      <body className="d-flex flex-column vh-100">
-        <Header theme={theme} toggleTheme={toggleTheme} />
-        <main className="flex-grow-1">{children}</main>
-        <Footer />
-      </body>
-    </html>
-  )
+    return (
+        <html
+            lang="en"
+            className={`${montserrat.variable} ${merriweather.variable}`}
+            data-bs-theme={theme}
+        >
+            <body className="d-flex flex-column vh-100">
+                <Header theme={theme} toggleTheme={toggleTheme} />
+                <main className="flex-grow-1">{children}</main>
+                <Footer />
+            </body>
+        </html>
+    );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,17 @@
-'use client'
-import { Container } from 'react-bootstrap'
-
-import data from '@data/contrarianmba.json'
+'use client';
+import { Container } from 'react-bootstrap';
 
 export default function Home() {
-  const bookIds = Object.keys(data.lookups.book_id)
-
-  return (
-    <Container>
-      <ul>
-        {data.categories.map((c) => (
-          <li>
-            {c}
-            <ul>
-              {bookIds.map((bookId) => (
-                <li>
-                  <Book bookId={bookId} />
-                </li>
-              ))}
-            </ul>
-          </li>
-        ))}
-      </ul>
-      <h1>Hello, World!</h1>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore
-        minima ea atque quas sunt laudantium, nihil suscipit reprehenderit non
-        doloremque dolorem totam hic sapiente rem culpa aut autem quibusdam.
-        Atque!
-      </p>
-      <div style={{ height: '2000px' }}></div>
-    </Container>
-  )
-}
-
-function Book({ bookId }) {
-  const book = data.lookups.book_id[bookId]
-  return (
-    <>
-      <a href={book.amazonProductURL}>
-        <img src={book.amazonImageURL} />
-        <br />
-        {book.title}
-      </a>
-      <br />
-      {book.summary}
-    </>
-  )
+    return (
+        <Container>
+            <h1>Hello, World!</h1>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Inventore minima ea atque quas sunt laudantium, nihil suscipit
+                reprehenderit non doloremque dolorem totam hic sapiente rem
+                culpa aut autem quibusdam. Atque!
+            </p>
+            <div style={{ height: '2000px' }}></div>
+        </Container>
+    );
 }
