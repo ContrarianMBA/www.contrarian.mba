7954ffb306a80c723da342813facbd48d31d589e
Yosef Ashenafi <jossyfreelancer@gmail.com>
2023-08-05 01:57:27 +0300

    Category navbar (#17)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,36 @@
 'use client';
-import { Container } from 'react-bootstrap';
+import { Container, Nav, Navbar, Offcanvas } from 'react-bootstrap';
 
 import { buildCategoryUrl } from '@/utils';
 import data from '@data/contrarianmba.json';
-import Link from 'next/link';
 
 export default function Home() {
     return (
-        <Container>
-            <div className="d-flex mt-3">
-                {data.categories.map((category) => (
-                    <Link
-                        className="category-labels"
-                        key={category}
-                        href={buildCategoryUrl(category)}
-                    >
-                        {category}
-                    </Link>
-                ))}
-            </div>
-        </Container>
+        <Navbar expand="lg" className="main-navbar">
+            <Container className="d-flex d-flex-end">
+                <Navbar.Toggle aria-controls="main-navbar-nav" />
+                <Navbar.Offcanvas id="main-navbar-nav" placement="end">
+                    <Offcanvas.Header closeButton>
+                        <Offcanvas.Title className="d-flex align-items-center">
+                            Categories
+                        </Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body>
+                        <Nav className="justify-content-end flex-grow-1 pe-3">
+                            {data.categories.map((category) => (
+                                <Nav.Link
+                                    className="category-labels link-body-emphasis"
+                                    key={category}
+                                    href={buildCategoryUrl(category)}
+                                >
+                                    {category}
+                                </Nav.Link>
+                            ))}
+                        </Nav>
+                        <Nav className="d-none d-md-flex align-items-center"></Nav>
+                    </Offcanvas.Body>
+                </Navbar.Offcanvas>
+            </Container>
+        </Navbar>
     );
 }
