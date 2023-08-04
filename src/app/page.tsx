172d0e7585ffb70048629b8c20041fae42e14591
jos <7629076+YosefAshenafi@users.noreply.github.com>
2023-08-05 00:41:28 +0300

    nav upate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,35 +3,34 @@ import { Container, Nav, Navbar, Offcanvas } from 'react-bootstrap';
 
 import { buildCategoryUrl } from '@/utils';
 import data from '@data/contrarianmba.json';
-import Link from 'next/link';
 
 export default function Home() {
     return (
-        <Container>
-            <Navbar expand="lg" className="main-navbar mobile-navbar">
-                <Container>
-                    <Navbar.Toggle aria-controls="main-navbar-nav" />
-                    <Navbar.Offcanvas id="main-navbar-nav" placement="end">
-                        <Offcanvas.Header closeButton>
-                            <Offcanvas.Title className="d-flex align-items-center"></Offcanvas.Title>
-                        </Offcanvas.Header>
-                        <Offcanvas.Body>
-                            <Nav className="justify-content-end flex-grow-1 pe-3">
-                                {data.categories.map((category) => (
-                                    <Link
-                                        className="category-labels"
-                                        key={category}
-                                        href={buildCategoryUrl(category)}
-                                    >
-                                        <Nav.Link href="/">{category}</Nav.Link>
-                                    </Link>
-                                ))}
-                            </Nav>
-                            <Nav className="d-none d-md-flex align-items-center"></Nav>
-                        </Offcanvas.Body>
-                    </Navbar.Offcanvas>
-                </Container>
-            </Navbar>
-        </Container>
+        <Navbar expand="lg" className="main-navbar">
+            <Container className="d-flex d-flex-end">
+                <Navbar.Toggle aria-controls="main-navbar-nav" />
+                <Navbar.Offcanvas id="main-navbar-nav" placement="end">
+                    <Offcanvas.Header closeButton>
+                        <Offcanvas.Title className="d-flex align-items-center">
+                            Categories
+                        </Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body>
+                        <Nav className="justify-content-end flex-grow-1 pe-3">
+                            {data.categories.map((category) => (
+                                <Nav.Link
+                                    className="category-labels link-body-emphasis"
+                                    key={category}
+                                    href={buildCategoryUrl(category)}
+                                >
+                                    {category}
+                                </Nav.Link>
+                            ))}
+                        </Nav>
+                        <Nav className="d-none d-md-flex align-items-center"></Nav>
+                    </Offcanvas.Body>
+                </Navbar.Offcanvas>
+            </Container>
+        </Navbar>
     );
 }
