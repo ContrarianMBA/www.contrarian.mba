56d0964f9fe760b1bb5fcf66c3d1c0a51ed25b03
Jonathan Tsai <jontsai@users.noreply.github.com>
2023-08-04 16:21:26 -0700

    fix URL building, and use `-`

--- src/utils/category.ts
+++ src/utils/category.ts
@@ -1,9 +1,9 @@
 export function buildCategoryUrl(category: string): string {
-    const value = category.replace(' ', '_');
+    const value = category.replace(/ /g, '-');
     return `/categories/${value}`;
 }
 
 export function getCategoryFromURLParam(urlParam: string): string {
-    const category = urlParam.replace('_', ' ');
+    const category = urlParam.replace(/-/g, ' ');
     return category;
 }
