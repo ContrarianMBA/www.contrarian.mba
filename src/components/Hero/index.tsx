0c877ca756df69ad2b0725aecc956139eb399fca
goztrk <me@gokhan.org.tr>
2023-08-04 19:55:48 +0300

    Change hero structure

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -7,24 +7,17 @@ export function Hero() {
             <Container className="my-5 py-5">
                 <Row>
                     <Col lg={6}>
-                        <p className="hero-text text-sm me-lg-5 pe-lg-5 mb-5 mb-lg-0">
+                        <p className="hero-text me-lg-5 pe-lg-5 mb-5 mb-lg-0">
                             An MBA is predominately a networking or career
-                            change exercise. The curriculum is weak and
-                            outdated, which follows from most of academia
-                            (except cutting-edge criticism). The following
-                            contrarian MBA curriculum introduces business
-                            principles, the economy, finance, political
-                            ramifications, and more through books by or about
-                            practitioners.
+                            change exercise. Across the board, the curriculum is
+                            weak and outdated, which follows from most of
+                            academia (except cutting-edge criticism). The
+                            following{' '}
+                            <span className="fw-bold h4">{SITE_NAME}</span>{' '}
+                            curriculum introduces business principles, the
+                            economy, finance, political ramifications, and more
+                            through books by or about practitioners.
                         </p>
-                    </Col>
-                    <Col
-                        lg={6}
-                        className="d-flex align-items-center justify-content-center"
-                    >
-                        <h1 className="hero-brand display-4 text-center">
-                            {SITE_NAME}
-                        </h1>
                     </Col>
                 </Row>
             </Container>
