651be7b223438a4de92b908ed4308f45d1a34713
goztrk <me@gokhan.org.tr>
2023-08-04 14:38:19 +0300

    update Hook

--- src/hooks/useColorMode.ts
+++ src/hooks/useColorMode.ts
@@ -1,20 +1,22 @@
 import React from 'react';
 import { ColorMode } from '@/types';
-import { DEFAULT_COLOR_MODE } from '../constants';
+import { COLOR_MODE_STORAGE_KEY, DEFAULT_COLOR_MODE } from '@/constants';
 
 export function useColorMode() {
-    const savedTheme =
-        typeof window !== 'undefined'
-            ? (window.localStorage.getItem('colorMode') as ColorMode)
-            : DEFAULT_COLOR_MODE;
-    const [colorMode, setColorMode] = React.useState<ColorMode>(
-        savedTheme || DEFAULT_COLOR_MODE,
-    );
+    const [colorMode, setColorMode] =
+        React.useState<ColorMode>(DEFAULT_COLOR_MODE);
+
+    React.useEffect(() => {
+        const savedColorMode = window.localStorage.getItem(
+            COLOR_MODE_STORAGE_KEY,
+        ) as ColorMode;
+        setColorMode(savedColorMode || DEFAULT_COLOR_MODE);
+    }, []);
 
     const toggleColorMode = () => {
         setColorMode(() => {
             const newMode = colorMode === 'light' ? 'dark' : 'light';
-            window.localStorage.setItem('colorMode', newMode);
+            window.localStorage.setItem(COLOR_MODE_STORAGE_KEY, newMode);
             return newMode;
         });
     };
