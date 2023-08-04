a4c62210664aa64933d686180108aacd527c1ab2
jos <7629076+YosefAshenafi@users.noreply.github.com>
2023-08-04 23:08:19 +0300

    page update to flex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,24 @@
 'use client';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 
+import { buildCategoryUrl } from '@/utils';
 import data from '@data/contrarianmba.json';
 import Link from 'next/link';
-import { buildCategoryUrl } from '@/utils';
 
 export default function Home() {
     return (
         <Container>
-            <Row>
+            <div className="d-flex mt-3">
                 {data.categories.map((category) => (
-                    <Col key={category}>
-                        <Link href={buildCategoryUrl(category)}>
-                            {category}
-                        </Link>
-                    </Col>
+                    <Link
+                        className="category-labels"
+                        key={category}
+                        href={buildCategoryUrl(category)}
+                    >
+                        {category}
+                    </Link>
                 ))}
-            </Row>
-            <h1>Hello, World!</h1>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Inventore minima ea atque quas sunt laudantium, nihil suscipit
-                reprehenderit non doloremque dolorem totam hic sapiente rem
-                culpa aut autem quibusdam. Atque!
-            </p>
-            <div style={{ height: '2000px' }}></div>
+            </div>
         </Container>
     );
 }
