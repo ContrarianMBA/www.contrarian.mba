03905d0443d83e4d0211a04c9421ac29e3939294
Jonathan Tsai <jontsai@users.noreply.github.com>
2023-08-05 12:03:44 -0700

    adds social urls to constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,5 @@
 export * from './affiliates';
 export * from './content';
 export * from './general';
+export * from './social';
 export * from './ui';
---
+++ src/constants/social.ts
@@ -0,0 +1,3 @@
+export const FACEBOOK_URL = 'https://facebook.com/ContrarianMBA';
+export const TWITTER_URL = 'https://twitter.com/Contrarian_MBA';
+export const INSTAGRAM_URL = 'https://instagram.com/ContrarianMBA';
