2373d9041a0228a2364d0e0e3bbec8de615622f9
Jonathan Tsai <jontsai@users.noreply.github.com>
2023-08-05 02:50:08 -0700

    show all books on home screen

--- src/app/categories/[slug]/page.tsx
+++ src/app/categories/[slug]/page.tsx
@@ -3,15 +3,11 @@ import { notFound } from 'next/navigation';
 import { Col, Container, Row } from 'react-bootstrap';
 
 import { BookID } from '@data/contrarianmba.json';
-import { BookCategoryView } from '@/components/Book';
 import { CategoryTitle } from '@/components/CategoryTitle';
+import { CategoryBooks } from '@/components/CategoryBooks';
 import { Book } from '@/types';
 import { CATEGORIES } from '@/constants';
-import {
-    lookUpBookById,
-    lookUpBookIdsByCategory,
-    resolveCategorySlug,
-} from '@/utils';
+import { resolveCategorySlug } from '@/utils';
 
 type Props = {
     params: {
@@ -20,26 +16,11 @@ type Props = {
 };
 export default function CategoryPage({ params: { slug } }: Props) {
     const category = resolveCategorySlug(slug);
-    const bookIds: string[] = lookUpBookIdsByCategory(category);
-    if (typeof bookIds === 'undefined') {
-        notFound();
-    }
-    const books: Book[] = bookIds.map((bookId: BookID) =>
-        lookUpBookById(bookId)
-    );
 
     return (
         <>
             <CategoryTitle category={category} />
-            <Container>
-                <Row>
-                    {books.map((book: Book) => (
-                        <Col lg={3} key={book.id}>
-                            <BookCategoryView book={book} />
-                        </Col>
-                    ))}
-                </Row>
-            </Container>
+            <CategoryBooks category={category} />
         </>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,30 @@
 'use client';
-import { Hero } from '@/components/Hero';
-import data from '@data/contrarianmba.json';
+
 import { Col, Container, Row } from 'react-bootstrap';
-import { BookCategoryView } from '../components/Book';
+
+import { CATEGORIES } from '@/constants';
+import { Hero } from '@/components/Hero';
+import { CategoryTitle } from '@/components/CategoryTitle';
+import { CategoryBooks } from '@/components/CategoryBooks';
 
 export default function Home() {
-    const books = Object.values(data.lookups.book_id);
-
     return (
         <>
             <Hero />
-            <Container>
-                <Row>
-                    {books.map((book) => (
-                        <Col key={book.id} lg={3}>
-                            <BookCategoryView book={book} />
-                        </Col>
-                    ))}
-                </Row>
-            </Container>
+            {CATEGORIES.map((category) => {
+                return (
+                    <p>
+                        <Container>
+                            <Row>
+                                <Col>
+                                    <h3>{category.name}</h3>
+                                </Col>
+                            </Row>
+                        </Container>
+                        <CategoryBooks category={category} />
+                    </p>
+                );
+            })}
         </>
     );
 }
