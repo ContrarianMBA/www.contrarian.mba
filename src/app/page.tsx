0a007abcd18b541fd8897450f87b9562222e9b3f
goztrk <me@gokhan.org.tr>
2023-08-05 03:45:07 +0300

    ✨ feat: display all books in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,24 @@
 'use client';
 import { Hero } from '@/components/Hero';
+import data from '@data/contrarianmba.json';
+import { Col, Container, Row } from 'react-bootstrap';
+import { BookCategoryView } from '../components/Book';
 
 export default function Home() {
-    return <Hero />;
+    const books = Object.values(data.lookups.book_id);
+
+    return (
+        <>
+            <Hero />
+            <Container>
+                <Row>
+                    {books.map((book) => (
+                        <Col key={book.id} lg={3}>
+                            <BookCategoryView book={book} />
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
+        </>
+    );
 }
