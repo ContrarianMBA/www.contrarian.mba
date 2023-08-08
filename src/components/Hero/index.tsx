12dd78a374cfe09ef3b4e3baac6f44de3e775a5a
Yosef Ashenafi <jossyfreelancer@gmail.com>
2023-08-08 23:56:56 +0300

    ✨ Share the site to social medias (#33)
    Co-authored-by: Jonathan Tsai <jontsai@users.noreply.github.com>

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,5 +1,7 @@
+import classNames from 'classnames';
 import { Col, Container, Row } from 'react-bootstrap';
-import { SITE_NAME } from '@/constants';
+import { HERO_BLURB } from '@/constants';
+import { HeroShareButtons } from '@/components/HeroShareButtons';
 
 export function Hero() {
     return (
@@ -7,17 +9,12 @@ export function Hero() {
             <Container className="my-5 py-5">
                 <Row>
                     <Col lg={6} className="mx-auto">
-                        <p className="hero-text me-lg-5 pe-lg-5 mb-5 mb-lg-0">
-                            An MBA is predominately a networking or career
-                            change exercise. Across the board, the curriculum is
-                            weak and outdated, which follows from most of
-                            academia (except cutting-edge criticism). The
-                            following{' '}
-                            <span className="fw-bold h4">{SITE_NAME}</span>{' '}
-                            curriculum introduces business principles, the
-                            economy, finance, political ramifications, and more
-                            through books by or about practitioners.
-                        </p>
+                        <p
+                            className="hero-text me-lg-5 pe-lg-5 mb-5 mb-lg-0"
+                            dangerouslySetInnerHTML={{ __html: HERO_BLURB }}
+                        ></p>
+
+                        <HeroShareButtons />
                     </Col>
                 </Row>
             </Container>
