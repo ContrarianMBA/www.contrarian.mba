b918d6ce80e3c74159e30787939df17a42171173
jos <7629076+YosefAshenafi@users.noreply.github.com>
2023-08-09 19:59:49 +0300

    mov consturctor to the top

--- src/types/category.ts
+++ src/types/category.ts
@@ -4,16 +4,16 @@ export class Category {
     name: string;
     slug: string;
 
+    constructor(categoryName: CategoryType) {
+        this.name = categoryName;
+        this.slug = categoryName.toLowerCase().replace(/ /g, '-');
+    }
+
     get path(): string {
         return `categories/${this.slug}`;
     }
 
     get url(): string {
         return `/${this.path}`;
-    }
-
-    constructor(categoryName: CategoryType) {
-        this.name = categoryName;
-        this.slug = categoryName.toLowerCase().replace(/ /g, '-');
     }
 }
