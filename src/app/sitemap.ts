f2cf6102f7a7f6992d9c13a8b3400fbee1c44459
jos <7629076+YosefAshenafi@users.noreply.github.com>
2023-08-09 18:34:36 +0300

    make the code DRY

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,9 +1,19 @@
 import { CATEGORIES, SITE_URL } from '@/constants';
 import { MetadataRoute } from 'next';
 
-export default function sitemap(): MetadataRoute.Sitemap {
-    return CATEGORIES.map((category) => ({
-        url: `${SITE_URL}${category.url}`,
+const createPages = (urls: string[]) => {
+    return urls.map((url) => ({
+        url: `${SITE_URL}${url}`,
         lastModified: new Date(),
     }));
+};
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    const staticPages = createPages(['', '/credits', '/jobs']);
+    const allPages = [
+        ...staticPages,
+        ...createPages(CATEGORIES.map((category) => category.url)),
+    ];
+
+    return allPages;
 }
