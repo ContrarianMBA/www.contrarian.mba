083974905a1ed7c47981bf1c7c11f545613683e6
jos <7629076+YosefAshenafi@users.noreply.github.com>
2023-08-09 20:01:43 +0300

    change path to url now

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,7 +6,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
 
     const dynamicPaths = [
         ...CATEGORIES.map((category) => {
-            return category.url;
+            return category.path;
         }),
     ];
 
