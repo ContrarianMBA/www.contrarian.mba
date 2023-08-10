cbfecf2075558897d95f47f7fe4fc5eb29083f9f
jos <7629076+YosefAshenafi@users.noreply.github.com>
2023-08-11 01:40:56 +0300

    fixed boo class

--- src/types/book.ts
+++ src/types/book.ts
@@ -8,16 +8,17 @@ export class Book implements BookType {
     category: CategoryType;
     bestInCategory: boolean;
     amazonProductID: string;
-    amazonImageURL: string;
     hasInventory: boolean;
 
     constructor(book: BookType) {
         Object.assign(this, book);
-
-        this.amazonImageURL = `/images/products/${this.amazonProductID}.jpg`;
     }
 
     get amazonProductURL(): string {
+        return `https://www.amazon.com/dp/${this.amazonProductID}?tag=${AMAZON_TRACKING_ID}&language=en_US`;
+    }
+
+    get amazonImageURL(): string {
         return `https://www.amazon.com/dp/${this.amazonProductID}?tag=${AMAZON_TRACKING_ID}&language=en_US`;
     }
 
